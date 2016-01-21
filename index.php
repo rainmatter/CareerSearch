46211406d19707cb53074b2f201b2984cebab7f4
Kim S. <stewak5@uw.edu>
2016-01-20 20:02:50 -0800

    Initial commit of new project.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CareerSearch version 2.0</title>
+    </head>
+    <body>
+        <h1>CareerSearch version 2.0</h1>
+        <?php
+          echo "New Career Search"
+        ?>
+    </body>
+</html>
